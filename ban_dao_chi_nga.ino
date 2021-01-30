0e9890b0f83043b885236caa68b829a270c4769f
vingocbk <vi.ngoc.bk@gmail.com>
2021-01-30 15:39:12 +0700

    first commit

---
+++ ban_dao_chi_nga.ino
@@ -0,0 +1,59 @@
+#include "ban_dao_chi_nga.h"
+
+void setup(void)
+{
+  Serial.begin(115200);
+	pinMode(RF0, INPUT);
+	pinMode(RF1, INPUT);
+	pinMode(RF2, INPUT);
+	pinMode(PIN_LOA, OUTPUT);
+	pinMode(MODE, INPUT);
+	pinMode(BUTTON_UP, INPUT);
+	pinMode(BUTTON_DOWN, INPUT);
+
+
+	// configure LED PWM functionalitites
+  	ledcSetup(L1UpChannel, freq, resolution);
+	ledcSetup(L1DownChannel, freq, resolution);
+	ledcSetup(L2UpChannel, freq, resolution);
+	ledcSetup(L2DownChannel, freq, resolution);
+  
+	// attach the channel to the GPIO to be controlled
+	ledcAttachPin(L1, L1UpChannel);
+	ledcAttachPin(L2, L1DownChannel);
+	ledcAttachPin(L3, L2UpChannel);
+	ledcAttachPin(L4, L2DownChannel);
+
+	ledcWrite(L1UpChannel, 0);
+	ledcWrite(L1DownChannel, 0);
+	ledcWrite(L2UpChannel, 0);
+	ledcWrite(L2DownChannel, 0);
+
+
+}
+
+void loop(void)
+{
+	if(digitalRead(BUTTON_UP) == LOW && digitalRead(BUTTON_DOWN) == HIGH){
+		ledcWrite(L1UpChannel, Max_L1_UP);
+		ledcWrite(L1DownChannel, 0);
+		ledcWrite(L2UpChannel, Max_L2_UP);
+		ledcWrite(L2DownChannel, 0);
+		delay(10);
+	}
+	else if(digitalRead(BUTTON_UP) == HIGH && digitalRead(BUTTON_DOWN) == LOW){
+		ledcWrite(L1UpChannel, 0);
+		ledcWrite(L1DownChannel, Max_L1_DOWN);
+		ledcWrite(L2UpChannel, 0);
+		ledcWrite(L2DownChannel, Max_L2_DOWN);  
+		delay(10);
+	}
+	else{
+		ledcWrite(L1UpChannel, 0);
+		ledcWrite(L1DownChannel, 0);
+		ledcWrite(L2UpChannel, 0);
+		ledcWrite(L2DownChannel, 0);
+		delay(10);
+	}
+
+}
